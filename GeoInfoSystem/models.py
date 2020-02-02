7983b5919131f5cd349237fbdcc7ace0ad229b17
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2020-02-02 17:58:45 +0100

    master: migrations done. First dbmodel done. Installed django rest framework.

--- GeoInfoSystem/models.py
+++ GeoInfoSystem/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
 # Create your models here.
+
+class puntInteres(models.Model):
+    latitud=models.FloatField()
+    logitud=models.FloatField()
+    idMapa=models.IntegerField()
+    tipus=models.CharField(max_length=700)
+    actiu=models.BooleanField()
+    superficie=models.FloatField()
+
+class usuari(models.Model):
+    nom=models.CharField(max_length=500)
+    cognom=models.CharField(max_length=500)
+    contrassenya=models.CharField(max_length=500)
+    correuElectronic=models.EmailField()
+    superUsuari=models.BooleanField()
+
+class local(models.Model):
+    localitzacio=models.ForeignKey(puntInteres, on_delete=models.CASCADE)
+    nomLocal=models.CharField(max_length=500)
+    puntuacio=models.IntegerField()
+    categoria=models.CharField(max_length=200)
+
+
