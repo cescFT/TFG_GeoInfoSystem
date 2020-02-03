89ba77c85be47c6df8580f697622e3272d86afe9
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2020-02-03 17:08:49 +0100

    master: working with admin part and modifications in models. migrations done.

--- GeoInfoSystem/admin.py
+++ GeoInfoSystem/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(puntInteres)
+admin.site.register(local)
+admin.site.register(usuari)
--- GeoInfoSystem/models.py
+++ GeoInfoSystem/models.py
@@ -3,13 +3,15 @@ from django.db import models
 
 class puntInteres(models.Model):
     latitud=models.FloatField()
-    logitud=models.FloatField()
+    longitud=models.FloatField()
     idMapa=models.IntegerField()
     tipus=models.CharField(max_length=700)
     actiu=models.BooleanField()
     superficie=models.FloatField()
     localitat=models.CharField(max_length=200)
     pais=models.CharField(max_length=200)
+    def __str__(self):
+        return "Punt interes localitzat:["+str(self.latitud)+","+str(self.longitud)+"] actiu="+str(self.actiu)+"de tipus:"+str(self.tipus)
 
 class usuari(models.Model):
     nom=models.CharField(max_length=500)
@@ -17,6 +19,8 @@ class usuari(models.Model):
     contrassenya=models.CharField(max_length=500)
     correuElectronic=models.EmailField()
     superUsuari=models.BooleanField()
+    def __str__(self):
+        return self.nom
 
 class local(models.Model):
     localitzacio=models.ForeignKey(puntInteres, on_delete=models.CASCADE)
@@ -25,5 +29,7 @@ class local(models.Model):
     categoria=models.CharField(max_length=200)
     anyConstruccio=models.IntegerField()
     descripcio=models.CharField(max_length=600)
+    def __str__(self):
+        return str(self.nomLocal)+" puntuacio="+str(self.puntuacio)+" categoria="+str(self.categoria)+" any construccio="+str(self.anyConstruccio)
 
 
