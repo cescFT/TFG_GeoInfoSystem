ed8c7d5f669da2bb5da1ce6946db63294b311558
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2020-06-22 13:17:14 +0200

    master: TFG completed.

--- GeoInfoSystem/urls.py
+++ GeoInfoSystem/urls.py
@@ -4,30 +4,6 @@ from django.urls import path
 from GeoInfoSystem import views
 
 urlpatterns = [
-    #API PUNT INTERES
-    path('allPuntsInteres/', views.getPuntsInteres, name='TotsPuntsInteres'),
-    path('puntInteresByCity/', views.getPuntInteresByCity, name='pInteresCiutat'),
-    path('puntInteresByCoordenades/', views.getPuntInteresEspecific, name='pInteresCoordenades'),
-    url(r'updatePuntInteres/(?P<latitud>\d+\.\d+)/(?P<longitud>\d+\.\d+)/$', views.updatePuntInteres, name='pInteresUpdate'),
-    path('newPuntInteres/', views.postNewPuntInteres, name='pInteresCreate'),
-    path('eliminarPuntInteres/', views.deletePuntInteres, name='pInteresEliminat'),
-    # API USUARIS
-    path('getUsuaris/', views.getUsuaris, name='usuaris'),
-    path('getSuperUsuaris/', views.getSuperUsuaris, name='superUsuaris'),
-    path('getUsuarisNormals/', views.getUsuarisNormals, name='usuaris Normals'),
-    path('getUsuari/<str:alias>/', views.getUsuariEspecific, name='usuari'),
-    path('updateUsuari/', views.updateUsuari, name='updateUsuari'),
-    path('createUsuari/', views.postNewUsuari, name='createNewUsuari'),
-    path('deleteUsuari/<str:alias>/', views.deleteUsuari, name='deleteUsuari'),
-    # API LOCALS
-    path('getLocals/', views.getLocals, name='allLocals'),
-    url(r'getLocalByLatLong/(?P<latitud>\d+\.\d+)/(?P<longitud>\d+\.\d+)/$', views.getLocalEspecificByLatLong, name='getLocalByLatLong'),
-    path('getLocalByName/<str:nomLocal>/', views.getLocalEspecificByName, name='getLocalByName'),
-    url(r'updateLocalByLatLong/(?P<nomLocal>[^/]+)/(?P<latitud>\d+\.\d+)/(?P<longitud>\d+\.\d+)/$', views.updateLocalByLatLong, name='updateLocalByLatLong'),
-    path('updateLocalByName/<str:nomLocal>/', views.updateLocalByName, name='updateLocalByName'),
-    path('createLocal/', views.postNewLocal, name='createLocal'),
-    path('deleteLocalByLatLong/', views.deleteLocalByLatLong, name='deleteByLatLong'),
-    path('deleteLocalByNom/', views.deleteLocalByName, name='deleteByName'),
 
     #############################################################
     #   VISTES DES DE LA WEB                                    #
